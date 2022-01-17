1fe232a0186f9cfc4ff0315dc98e072dd736bb76
miloszzaj <74071184+miloszzaj@users.noreply.github.com>
2022-01-17 12:34:04 +0100

    header added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import Section from "./Section";
+import Header from "./Header";
 
 const tasks = [
   { id: 1, content: "test1", isDone: false },
@@ -12,15 +13,19 @@ const hideDoneTasks = false;
 
 function App() {
   return (
-    <main className="todolist">
-      <header className="todolist__header">Lista zadań</header>
-      <Section title="Dodaj nowe zadanie" body={<Form />} />
-      <Section
-        title="Lista zadań"
-        body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks}></Tasks>}
-        headerContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />}
-      />
-    </main>
+    <body className="body">
+      <main className="todolist">
+        <Header title="Lista zadań" />
+        <Section title="Dodaj nowe zadanie" body={<Form />} />
+        <Section
+          title="Lista zadań"
+          body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks}></Tasks>}
+          headerContent={
+            <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
+          }
+        />
+      </main>
+    </body>
   );
 }
 
---
+++ src/Header/index.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+const Header = ({ title }) => {
+  return <header className="todolist__header">{title}</header>;
+};
+
+export default Header;
