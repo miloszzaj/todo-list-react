0a9a47c606461648f763f4f7f4bb613fbc2ad706
miloszzaj <74071184+miloszzaj@users.noreply.github.com>
2022-01-26 17:21:15 +0100

    block to add empty task

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,28 +1,28 @@
-import { useState } from "react";
-import "./style.css";
+import { useState } from 'react';
+import './style.css';
 
 const Form = ({ addNewTask }) => {
-  const [newTaskContent, setNewTaskContent] = useState("");
+	const [newTaskContent, setNewTaskContent] = useState('');
 
-  const onFormSubmit = e => {
-    e.preventDefault();
-    setNewTaskContent("");
-    return newTaskContent !== "" ? addNewTask(newTaskContent.trim()) : null;
-  };
+	const onFormSubmit = e => {
+		e.preventDefault();
+		setNewTaskContent('');
+		return newTaskContent.trim() !== '' ? addNewTask(newTaskContent.trim()) : null;
+	};
 
-  return (
-    <form className="adding__paragraph" onSubmit={onFormSubmit}>
-      <input
-        value={newTaskContent}
-        onChange={({ target }) => setNewTaskContent(target.value)}
-        className="adding__input"
-        type="text"
-        placeholder="Co jest do zrobienia?"
-        autoFocus
-      />
-      <button className="adding__button">Dodaj zadanie</button>
-    </form>
-  );
+	return (
+		<form className='adding__paragraph' onSubmit={onFormSubmit}>
+			<input
+				value={newTaskContent}
+				onChange={({ target }) => setNewTaskContent(target.value)}
+				className='adding__input'
+				type='text'
+				placeholder='Co jest do zrobienia?'
+				autoFocus
+			/>
+			<button className='adding__button'>Dodaj zadanie</button>
+		</form>
+	);
 };
 
 export default Form;
