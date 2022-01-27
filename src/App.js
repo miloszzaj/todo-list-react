801c6e0c09eacec42b5964d42c36641d6314867d
miloszzaj <74071184+miloszzaj@users.noreply.github.com>
2022-01-27 17:57:29 +0100

    localStorage added

--- src/App.js
+++ src/App.js
@@ -1,85 +1,70 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
 
-import Form from "./Form";
-import Tasks from "./Tasks";
-import Buttons from "./Buttons";
-import Section from "./Section";
-import Header from "./Header";
-import Container from "./Container";
+import Form from './Form';
+import Tasks from './Tasks';
+import Buttons from './Buttons';
+import Section from './Section';
+import Header from './Header';
+import Container from './Container';
 
 function App() {
-  const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      content: "test 1",
-      isDone: false,
-    },
-  ]);
+	const [hideDone, setHideDone] = useState(false);
 
-  const removeTask = id => {
-    setTasks(tasks => tasks.filter(task => task.id !== id));
-  };
+	const storedTasks = localStorage.getItem('tasks');
+	const [tasks, setTasks] = useState(storedTasks !== null ? JSON.parse(storedTasks) : []);
 
-  const toggleTaskDone = id => {
-    setTasks(tasks =>
-      tasks.map(task => {
-        if (task.id === id) {
-          return { ...task, isDone: !task.isDone };
-        } else {
-          return task;
-        }
-      })
-    );
-  };
+	useEffect(() => {
+		localStorage.setItem('tasks', JSON.stringify(tasks));
+	}, [tasks]);
 
-  const toggleHideDone = () => {
-    setHideDone(hideDone => !hideDone);
-  };
+	const removeTask = id => {
+		setTasks(tasks => tasks.filter(task => task.id !== id));
+	};
 
-  const setAllDone = () => {
-    setTasks(tasks => tasks.map(task => ({ ...task, isDone: true })));
-  };
+	const toggleTaskDone = id => {
+		setTasks(tasks =>
+			tasks.map(task => {
+				if (task.id === id) {
+					return { ...task, isDone: !task.isDone };
+				} else {
+					return task;
+				}
+			})
+		);
+	};
 
-  const addNewTask = newTaskContent => {
-    setTasks(tasks => [
-      ...tasks,
-      {
-        content: newTaskContent,
-        isDone: false,
-        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
-      },
-    ]);
-  };
+	const toggleHideDone = () => {
+		setHideDone(hideDone => !hideDone);
+	};
 
-  return (
-    <Container>
-      <Header title="Lista zadań" />
-      <Section
-        title="Dodaj nowe zadanie"
-        body={<Form addNewTask={addNewTask} />}
-      />
-      <Section
-        title="Lista zadań"
-        body={
-          <Tasks
-            tasks={tasks}
-            hideDone={hideDone}
-            removeTask={removeTask}
-            toggleTaskDone={toggleTaskDone}
-          />
-        }
-        headerContent={
-          <Buttons
-            tasks={tasks}
-            hideDone={hideDone}
-            toggleHideDone={toggleHideDone}
-            setAllDone={setAllDone}
-          />
-        }
-      />
-    </Container>
-  );
+	const setAllDone = () => {
+		setTasks(tasks => tasks.map(task => ({ ...task, isDone: true })));
+	};
+
+	const addNewTask = newTaskContent => {
+		setTasks(tasks => [
+			...tasks,
+			{
+				content: newTaskContent,
+				isDone: false,
+				id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
+			},
+		]);
+	};
+
+	return (
+		<Container>
+			<Header title='Lista zadań' />
+			<Section title='Dodaj nowe zadanie' body={<Form addNewTask={addNewTask} />} />
+			<Section
+				title='Lista zadań'
+				body={<Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} toggleTaskDone={toggleTaskDone} />}
+				headerContent={
+					<Buttons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} setAllDone={setAllDone} />
+				}
+			/>
+		</Container>
+	);
 }
 
 export default App;
