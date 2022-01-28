4929b1a9f1a64c2cfca4cc18d40091a3e1240652
miloszzaj <74071184+miloszzaj@users.noreply.github.com>
2022-01-28 21:03:55 +0100

    toggleTaskDone changed

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,35 +1,19 @@
-import "./style.css";
+import './style.css';
 
 const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
-  <ul className="list__tasks">
-    {tasks.map(task => (
-      <li
-        key={task.id}
-        className={`
-        list__item ${task.isDone && hideDone ? "list__item--hidden" : ""}`}
-      >
-        <button
-          onClick={() => toggleTaskDone(task.id)}
-          className="list__itemButton"
-        >
-          {task.isDone ? "✓" : ""}
-        </button>
-        <span
-          className={`list__item--span ${
-            task.isDone ? "list__item--done" : ""
-          }`}
-        >
-          {task.content}
-        </span>
-        <button
-          onClick={() => removeTask(task.id)}
-          className="list__itemButton list__itemButton--delete"
-        >
-          🗑
-        </button>
-      </li>
-    ))}
-  </ul>
+	<ul className='list__tasks'>
+		{tasks.map(task => (
+			<li key={task.id} className={`ist__item ${task.isDone && hideDone ? 'list__item--hidden' : ''}`}>
+				<button onClick={() => toggleTaskDone(task.id)} className='list__itemButton'>
+					{task.isDone ? '✓' : ''}
+				</button>
+				<span className={`list__item--span ${task.isDone ? 'list__item--done' : ''}`}>{task.content}</span>
+				<button onClick={() => removeTask(task.id)} className='list__itemButton list__itemButton--delete'>
+					🗑
+				</button>
+			</li>
+		))}
+	</ul>
 );
 
 export default Tasks;
