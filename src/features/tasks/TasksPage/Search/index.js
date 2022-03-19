4379a8a9e8cee5afbcd7e40f3a48abf2a34032ea
miloszzaj <zajaczkowski.milosz@gmail.com>
2022-03-19 20:50:33 +0100

    useHistory added

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,18 +1,19 @@
 import { useHistory, useLocation } from 'react-router-dom';
 import Input from '../../Input';
+import searchQueryParamName from '../searchQueryParamName';
 import { Wrapper } from './styled';
 
 const Search = () => {
 	const location = useLocation();
 	const history = useHistory();
-	const query = new URLSearchParams(location.search).get('szukaj');
+	const query = new URLSearchParams(location.search).get(searchQueryParamName);
 
 	const onInputChange = ({ target }) => {
 		const searchParams = new URLSearchParams(location.search);
 		if (target.value.trim() === '') {
-			searchParams.delete('szukaj');
+			searchParams.delete(searchQueryParamName);
 		} else {
-			searchParams.set('szukaj', target.value);
+			searchParams.set(searchQueryParamName, target.value);
 		}
 
 		history.push(`${location.pathname}?${searchParams.toString()}`);
