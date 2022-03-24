5fd91c7dfc29c8017fd149e2dfbc6474444fa383
miloszzaj <zajaczkowski.milosz@gmail.com>
2022-03-24 12:32:50 +0100

    cursor: pointer added to buttons

--- src/features/tasks/TasksListButtons/index.js
+++ src/features/tasks/TasksListButtons/index.js
@@ -1,35 +1,35 @@
-import { Wrapper, Button } from '../../../common/StyledButton/styledButtons';
+import { Wrapper, Button } from '../TasksPage/StyledButtons/styledButtons';
 import { useSelector, useDispatch } from 'react-redux';
 import {
-	toggleHideDone,
-	setAllDone,
-	selectHideDone,
-	selectIsEveryTaskDone,
-	selectAreTasksEmpty,
+  toggleHideDone,
+  setAllDone,
+  selectHideDone,
+  selectIsEveryTaskDone,
+  selectAreTasksEmpty,
 } from '../../tasks/tasksSlice';
 
 const TasksListButtons = () => {
-	const hideDone = useSelector(selectHideDone);
-	const isEveryTaskDone = useSelector(selectIsEveryTaskDone);
-	const areTasksEmpty = useSelector(selectAreTasksEmpty);
+  const hideDone = useSelector(selectHideDone);
+  const isEveryTaskDone = useSelector(selectIsEveryTaskDone);
+  const areTasksEmpty = useSelector(selectAreTasksEmpty);
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	return (
-		<Wrapper>
-			{!areTasksEmpty && (
-				<>
-					<Button onClick={() => dispatch(toggleHideDone())}>
-						{hideDone ? 'Pokaż ' : 'Ukryj '}
-						ukończone
-					</Button>
-					<Button onClick={() => dispatch(setAllDone())} disabled={isEveryTaskDone}>
-						Ukończ wszystkie
-					</Button>
-				</>
-			)}
-		</Wrapper>
-	);
+  return (
+    <Wrapper>
+      {!areTasksEmpty && (
+        <>
+          <Button onClick={() => dispatch(toggleHideDone())}>
+            {hideDone ? 'Pokaż ' : 'Ukryj '}
+            ukończone
+          </Button>
+          <Button onClick={() => dispatch(setAllDone())} disabled={isEveryTaskDone}>
+            Ukończ wszystkie
+          </Button>
+        </>
+      )}
+    </Wrapper>
+  );
 };
 
 export default TasksListButtons;
--- src/features/tasks/TasksPage/StyledButtons/styledButtons.js
+++ src/features/tasks/TasksPage/StyledButtons/styledButtons.js
@@ -1,37 +1,37 @@
 import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: center;
-	@media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-		flex-direction: column;
-		padding-top: 20px;
-	}
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    flex-direction: column;
+    padding-top: 20px;
+  }
 `;
 
 export const Button = styled.button`
-	border: none;
-	background-color: transparent;
-	color: ${({ theme }) => theme.color.teal};
-	font-size: 0.9rem;
-	transition: color 0.5s ease;
+  border: none;
+  background-color: transparent;
+  color: ${({ theme }) => theme.color.teal};
+  font-size: 0.9rem;
+  transition: color 0.5s ease;
+  cursor: pointer;
 
-	${({ disabled }) =>
-		disabled &&
-		css`
-			color: ${({ theme }) => theme.color.doveGray};
-			cursor: not-allowed;
-		`}
+  ${({ disabled }) =>
+    disabled &&
+    css`
+      color: ${({ theme }) => theme.color.doveGray};
+      cursor: not-allowed;
+    `}
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    margin: 10px;
+  }
 
-	@media (max-width:${({ theme }) => theme.breakpoint.mobile}px) {
-		margin: 10px;
-	}
-
-	&:hover {
-		color: ${({ theme }) => theme.color.pacificBlue};
-	}
-	&:disabled {
-		color: ${({ theme }) => theme.color.doveGray};
-	}
+  &:hover {
+    color: ${({ theme }) => theme.color.pacificBlue};
+  }
+  &:disabled {
+    color: ${({ theme }) => theme.color.doveGray};
+  }
 `;
